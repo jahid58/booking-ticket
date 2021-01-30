3ea69d0ca175aa6ac3b552dc2606134a395ddc11
Jahid Hasan <jahidjjaa@gmail.com>
2021-01-30 19:58:57 +0600

    second addition

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //converting string to num function
  function getStrToNum(id){
     let str = document.getElementById(id);
-     let strNum = parseInt(str.value);
+    let strNum = parseInt(str.value);
      return strNum;
  }
 
@@ -9,7 +9,7 @@
 function calculateTotal(ticketType,isIncrease){
 
     let ticketInput =document.getElementById(ticketType +'-ticket');
-    let ticketCount = getStrToNum(ticketType +'-ticket')
+    let ticketCount = getStrToNum(ticketType +'-ticket');
 
     //increasing & decreasing ticket count
     if (isIncrease == true) {
@@ -23,33 +23,32 @@ function calculateTotal(ticketType,isIncrease){
 
     // calculating ticket cost
     let firstClassTicketCount = getStrToNum('first-class-ticket');
-    let economyTicketCount = getStrToNum('economy-ticket')
-    let subtotal = firstClassTicketCount * 150 + economyTicketCount *100
+    let economyTicketCount = getStrToNum('economy-ticket');
+    let subtotal = firstClassTicketCount * 150 + economyTicketCount *100 ;
 
       // showing subtotal, vat and total
     document.getElementById('subtotal').innerText = subtotal;
     document.getElementById('vat').innerText = subtotal * 0.1;
-    document.getElementById('total').innerText = subtotal +(subtotal * 0.1)
-
+    document.getElementById('total').innerText = subtotal +(subtotal * 0.1);
  }
 
  
  //booking confirm page
  function booked(){
     //  styling
-     let homePage = document.getElementById('home-page')
+     let homePage = document.getElementById('home-page');
      let secondPage = document.getElementById('success-page');
-     homePage.style.display = 'none'
-     secondPage.style.display = 'block'
+     homePage.style.display = 'none';
+     secondPage.style.display = 'block';
 
     //  collect and showing details
      let fClassTicketNum = getStrToNum('first-class-ticket');
      let ecoTicketNum = getStrToNum('economy-ticket');
      let total = document.getElementById('total').innerText;
      document.getElementById('details').innerHTML = 'You have booked <br>'+
-      ' First class ticket  : '+fClassTicketNum +
-     '<br> Economy ticket : '+ ecoTicketNum +
-      ' <br>  Total cost :  $'+ total;
+        ' First class ticket  : '+fClassTicketNum +
+        '<br> Economy ticket : '+ ecoTicketNum +
+        ' <br>  Total cost :  $'+ total;
  }
 
  
